b173ddea5436349d41de6e6a51cb62e189283359
hazelkozlu <hazelkozlu43@gmail.com>
2022-09-10 13:03:09 +0300

    Add project files.

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,47 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext : DbContext
+    {
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<Hotel>().HasData(new Hotel
+        //    {
+        //        Id = 1,
+        //        Name = "Çınar Hotel",
+        //        City = "New York",
+              
+
+        //    });
+        //    modelBuilder.Entity<Hotel>().HasData(new Hotel
+        //    {
+        //        Id = 2,
+        //        Name = "Assosia Hotel",
+        //        City = "New York",
+
+
+        //    });
+        //}
+        //HotelFinder.DataAccess a entityframeworkcore sqlserver ve tools paketlerini indirdik manage nugetten
+        //migration lar için entityframeworkcore.tools u indirdik
+        //migration için dataaccess projesinin pmconsole da seçili olması ve set as startuo project denmesi gerekiyor.
+        //migration ekle add-migration InitialCreate update-database 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //"Server=DESKTOP-2ID4KL6\SQLEXPRESS;Database=PersonnelDB;Integrated Security=true"
+            optionsBuilder.UseSqlServer("Server=DESKTOP-2ID4KL6;Database=HotelDB;Integrated Security=true");
+
+        }
+        //bu Entities class library sindeki classlar benim sql imdeki tablolar
+        public DbSet<Hotel> Hotels { get; set; }
+        //budemek oluyorki bussines katmanında HotelDbContext i new leyince Hotels diye bir property gelecek
+        //oda dbset türünden olduğu için dbset teki add, addrance,update gibi methotlara uşamış olacağım
+    }
+}
---
+++ HotelFinder.DataAccess/Migrations/20220910093648_NewAutoDatas.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelFinder.DataAccess.Migrations
+{
+    public partial class NewAutoDatas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "City", "Name" },
+                values: new object[] { 1, "New York", "Çınar Hotel" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "City", "Name" },
+                values: new object[] { 2, "New York", "Assosia Hotel" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
